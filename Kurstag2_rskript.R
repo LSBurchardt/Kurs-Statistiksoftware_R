c20caaa8335387fdb2a333ff1499cf40ef5ea921
LSBurchardt <l.s.burchardt@gmx.de>
2019-05-26 17:18:44 +0200

    Kurstag 1 & 2 ergänzt um im Kurs besprochene Beispiele

--- Kurstag2_rskript.R
+++ Kurstag2_rskript.R
@@ -5,10 +5,13 @@
 #24.05/25.05
 #21.06/22.06
 #
+#2. Kurstag, behandelt wurden folgende Themen:
+#   - Good Coding Practice 
+#   - Datenmanagment
+#   - Kreutabellen
+#   - lineare Regression und Korrelation
+#   - t-test und ANOVA
 ################################################################
-
-
-#2. Kurstag
 
 # 00: Pakete laden ---------------------------------------------------------------
 
@@ -22,39 +25,36 @@ library(ggplot2)
 
 # 02: Good Coding Practice und Github ----
 
-      #...
+      #siehe Präsentation Folien 32-34
+
 # Github
 # um ein Paket von Github zu laden brauchen wir das Paket "devtools"
+
 install.packages("devtools")
 
-#dann kann man Pakete von Github laden
+#damit können Pakete von Github geladen werden, hier ein Beispiel
+
 devtools::install_github("kassambara/ggpubr")   #ggpubr: Publication ready plots
 
-# 03: Datenmanagment-----
+# 03: Datenmanagment---------------------------------------------------
 
 # 03a: Teilmengen bilden
 
-#Teilmenge der Studierenden aus Berlin
+#Teilmenge der Studierenden aus Berlin, Funktion: filter()
 
 berliner<-erstis %>% 
   filter(berlin == "ja")
 
-#Teilmenge: Variablen "uni"
+#Teilmenge: Variablen "uni", Funktion: select()
 
 var_uni<-erstis %>% 
   select(code,uni1,uni2,uni3,uni4,uni5,uni6,uni7)
 
-# 03b: Gruppieren und sortieren
-
-#Wofür kann die Funktion group_by() gebraucht werden?
+# 03b: Gruppieren und sortieren, Funktionen group_by() und arrange()
 
 erstis %>% 
   group_by(gruppe) %>% 
-  summarise_at(vars(zuf.inh.1), funs(mean(.,na.rm = TRUE)))
-
-erstis %>% 
-  group_by(gruppe) %>% 
-  summarise_at(vars(alter), list(~mean(.,na.rm=TRUE)))
+  summarise_at(vars(alter), list(~mean(.,na.rm=TRUE), ~min(.,na.rm= TRUE, ~max(.,na.rm = TRUE))))
 
 
 #Sortiert den Datensatz nach Gruppe und Alter
@@ -62,48 +62,41 @@ erstis %>%
 nach_gruppe_alter<-erstis %>% 
   arrange(gruppe, alter)
 
-#gruppieren nach gruppe, mittelwert von alter
-
-erstis %>% 
-     group_by(gruppe) %>% 
-     summarise_at(vars(alter), list(~mean(.,na.rm=TRUE)))
 
 # NAs filtern um Alter nach Geschlecht darzustellen
 
 mean_nach_alter_geschlecht_2<-erstis %>% 
-  filter(geschl == "weiblich" | geschl == "männlich") %>%  
-  #filter(!is.na(geschl)) %>%    #!is.na bedeutet: ist kein "missing value"
+  filter(geschl == "weiblich" | geschl == "männlich") %>%  # | ist der ODER Operator
+  filter(!is.na(geschl)) %>%    #!is.na bedeutet: ist kein "missing value"
     group_by(geschl) %>% 
     summarise_at(vars(alter), list(~mean(.,na.rm= TRUE)))
 
+
+#andere Variante: erst neuen Datensatz erstellen, dann mean bilden
 frauen<-erstis %>% 
   filter(geschl == "weiblich")
 
 frauen.alter.mean<-frauen$alter %>% 
   mean(.,na.rm=TRUE)
-#chaining
-
 
 
 # 03c: Datensätze kombinieren
 
-bind_rows()
+bind_rows() #verbindet Beobachtungen
 
-bind_cols()
+bind_cols() #verbindet Variablen
 
-erstis_na_omit<-na.omit(erstis)     #alle Zeilen, die NAs enthalten, werden rausgeschmissen
-mean_erstis_na_omit<-summarise_all(erstis_na_omit, funs(mean)) #"mean" wird auf alle Spalten angewendet
 
 # 04 Kreuztabellen ----------------------------------------------------------
 
-attach(erstis)
+attach(erstis)    #über die attach Funktion fügt Datensatz zu Rs "search path" hinzu,
+                  #Variablen können in Funktionen direkt angesprochen werden 
 
 #absolute Häufigkeiten
-table1<-table(berlin, geschl, useNA = "ifany")
+table1<-table(berlin, geschl, useNA = "ifany")  #useN= "ifany" löscht Zeilen, in denen mind. 1 Variable NAs enthalten
 addmargins(table(berlin, geschl))
 
 #relative Häufigkeiten
-
 table2<-prop.table(table(berlin, geschl, useNA = "ifany"))
 addmargins(prob.table(table(geschl,berlin)))
 
@@ -114,7 +107,7 @@ tab_berlin_geschl<-table(berlin, geschl)
 tab_berlin_geschl %>% 
 prop.table() %>% 
   addmargins() %>% 
-  print(digits= 3)
+  print(digits= 3)    #Dezimalstellen im Output werden auf 3 digits begrenzt
 
 #Variante2
 table(berlin,geschl) %>% 
@@ -129,13 +122,18 @@ x <- c(-2, -1, -0.8, -0.3, 0, 0.5, 0.6, 0.7, 1, 1.2)
 y <- c(1.9, 0.6, 0.5, 0.8, -0.4, -0.9, -0.7, -0.1, -1.7, -0.2)
 
 # lineare Regression mit lm()
-plot(x,y)
 fm<-lm(y~x)  #fm = fittet model, mit der Funktion lm (linear model) wird die lineare Regression berechnet
              # sie wird berechnet für die Formel y~x, in der y durch x erklärt werden soll 
 
 plot(x,y,xlim = c(-3,3), ylim = c(-3,3), pch= 19)   #Daten werden erneut geplotet 
 abline(fm,col = "red" )                             # und um die Trendlinie erweitert
 fm2<-lm(alter~ abi)
+
+#ggplot mit Trendlinie
+
+ggplot(erstis, aes(x= alter, y= zuf.inh.1))+
+  geom_point(color= "skyblue", shape = "square")+
+  geom_smooth(method = 'lm', se = FALSE) 
 
 #Korrelation mit cor() und corr.test()
 xy<-data.frame(x,y)         # wir wollen wissen wie stark der Zusammenghang ist und rechnen eine Korrelation
@@ -146,15 +144,7 @@ cor2<-corr.test(xy)         # Version der Pearson-Korrelation aus dem "psych" Pa
 print(cor2)                 # es können Stichprobengrößen und p Werte ausgegeben werden 
 print(cor2, short = FALSE)
 
-# Korrelation zwischen den Variablen abi, alter und zuf.inh.1
-# plottet die Variablen, alter gegen zuf.inh.1 und abi gegen zuf.inh.1
-plot()
 
-ggplot(erstis, aes(x= alter, y= zuf.inh.1))+
-  geom_point(color= "skyblue", shape = "square")+
-  geom_smooth(method = 'lm', se = FALSE)
-  
-  
 #führt eine Korrelationsanalyse durch
 auswahl<-data.frame(alter, abi, zuf.inh.1)
 cor(auswahl)
